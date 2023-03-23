20d35ca6c3d0a77675c1f26419f5972182dc02c7
ryan2414 <jackpar@nate.com>
2023-03-23 23:58:21 +0900

    Study Event

--- StudyReact/src/App.tsx
+++ StudyReact/src/App.tsx
@@ -1,6 +1,29 @@
-import P from './P'
+import EventListener from './pages/EventListener'
+import OnClick from './pages/OnClick'
+import ReactOnClick from './pages/ReactOnClick'
+import DispatchEvent from './pages/DispatchEvent'
+import EventBubbling from './pages/EventBubbling'
+import StopPropagation from './pages/StopPropagation'
+import VariousImputs from './pages/VariousInputs'
+import OnChange from './pages/OnChange'
+import FileInput from './pages/FileInput'
+import DrageDrop from './pages/DragDrop'
+import FileDrop from './pages/FileDrop'
 
 export default function App() {
-  const texts = ['hello', 'world'].map((text, index) => <P key={index} children={text} />)
-  return <div children={texts} />
+  return (
+    <div>
+      <FileDrop />
+      <DrageDrop />
+      <FileInput />
+      <OnChange />
+      <VariousImputs />
+      <StopPropagation />
+      <EventBubbling />
+      {/* <DispatchEvent />
+      <ReactOnClick />
+      <OnClick />
+      <EventListener /> */}
+    </div>
+  )
 }
